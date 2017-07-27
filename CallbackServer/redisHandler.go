e2f1a3879151f4ccc17f55cfbdc1bdedaeccaa7d
heshanindika <k.heshan.indika@gmail.com>
2017-07-27 18:04:26 +0530

    auth security db

--- CallbackServer/redisHandler.go
+++ CallbackServer/redisHandler.go
@@ -661,6 +661,7 @@ func SecurityGet(key string) string {
 
 	if redisMode == "sentinel" {
 		client, err = sentinelPool.GetMaster(redisClusterName)
+		client.Cmd("select", "0")
 		errHndlr(err)
 		defer sentinelPool.PutMaster(redisClusterName, client)
 	} else {
